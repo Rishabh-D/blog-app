d8ceccf6f8f1f759ea0b85a8ca96db0693fc4843
RISHABH <dubeyrishabh.rd@gmail.com>
2021-02-22 14:07:44 +0530

    skeleton pages created. Dummy data added

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const app = express();
+
+// registering view engine for express
+
+app.set("view engine", "ejs");
+app.set("views", "pages");
+
+//listening for requests
+port = 8080;
+host = "127.0.0.1";
+app.listen(port, host, () => {
+  console.log(`listening at ${host}` + ":\\" + `${port}`);
+});
+
+//serving, routing
+
+app.get("/", (req, res) => {
+  //   the directory specified is relative. But node looks for it in the root
+  //   folder of the system. So we add a object as a parameter and specify root as the project folder
+  //   res.sendFile("./pages/index.html", { root: __dirname });
+
+  //rendering view
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// if user accidently types `about-us` insted of `about` in the url then we redirect him {res.redirect('/validUrl')}
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+
+// 404, krrp this line at the end always
+app.use((req, res) => {
+  res.status(404);
+  res.render("notFound404");
+});
